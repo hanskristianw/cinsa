fe458e4de23d6d0c67e04c8a4b6b21a22ae15aaf
Hans Kristian W <hanskristianwijaya@yahoo.com>
2019-04-24 15:06:34 +0700

    rubah warna

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,96 +12,59 @@
       <!-- Divider -->
       <hr class="sidebar-divider">
 
-    <!-- QUERY MENU -->
+    <!-- MANAJEMEN MENU -->
     <?php
-      // $role_id = $this->session->userdata('role_id');
 
-      // $queryMenu = "SELECT u_id, u_menu
-      //               FROM u
-      //               JOIN u_access
-      //               ON u_id = u_sub_u_id
-      //               WHERE u_access_u_id = $role_id
-      //               ORDER BY u_access_u_sub_id ASC";
+      if($this->session->userdata('kr_jabatan_id')==1 && $this->session->userdata('kr_jabatan_id')){
+        //Administrator
+        echo'<div class="sidebar-heading">Administrator</div>
+            <li class="nav-item">
+              <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Master</span>
+              </a>
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                  <h6 class="collapse-header">Master:</h6>
+                  <a class="collapse-item" href="buttons.html">Tahun Ajaran</a>
+                  <a class="collapse-item" href="cards.html">Semester</a>
+                  <a class="collapse-item" href="cards.html">Sekolah</a>
+                  <a class="collapse-item" href="cards.html">Jabatan</a>
+                  <a class="collapse-item" href="cards.html">Struktur</a>
+                </div>
+              </div>
+            </li>
 
-      // $menu = $this->db->query->result
+            <hr class="sidebar-divider">';
+      }else{
+        echo'<div class="sidebar-heading">
+              Karyawan
+            </div>
+
+            <li class="nav-item">
+              <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                <i class="fas fa-fw fa-cog"></i>
+                <span>Components</span>
+              </a>
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                  <h6 class="collapse-header">Custom Components:</h6>
+                  <a class="collapse-item" href="buttons.html">Buttons</a>
+                  <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+              </div>
+            </li>
+
+            <hr class="sidebar-divider d-none d-md-block">';
+      }
     ?>
 
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Administrator
-      </div>
 
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('Home') ?>">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
-      </li>
 
       <!-- Divider -->
-      <hr class="sidebar-divider">
 
       <!-- Heading -->
-      <div class="sidebar-heading">
-        Karyawan
-      </div>
 
-      <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
-          </div>
-        </div>
-      </li>
-
-      <!-- Nav Item - Utilities Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-          <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
-          </div>
-        </div>
-      </li>
-
-      <!-- Divider -->
-      <hr class="sidebar-divider">
-
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Addons
-      </div>
-
-      <!-- Nav Item - Charts -->
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
-      </li>
-
-      <!-- Nav Item - Tables -->
-      <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
-      </li>
-
-      <!-- Divider -->
-      <hr class="sidebar-divider d-none d-md-block">
 
       <!-- Sidebar Toggler (Sidebar) -->
       <div class="text-center d-none d-md-inline">
