47593987ac21dbfd9628c0dce761d675d0c67610
Hans Kristian <hanskristianwijaya@yahoo.com>
2019-04-29 22:55:42 +0700

    insert karyawan

--- application/controllers/Karyawan_CRUD.php
+++ application/controllers/Karyawan_CRUD.php
@@ -7,10 +7,11 @@ class Karyawan_CRUD extends CI_Controller
   {
     parent::__construct();
     $this->load->model('_kr');
+    $this->load->model('_jabatan');
 
     //jika belum login
     if(!$this->session->userdata('kr_jabatan_id')){
-      redirect('auth');
+      redirect('Auth');
     }
 
     //jika bukan HRD dan sudah login redirect ke home
@@ -24,7 +25,7 @@ class Karyawan_CRUD extends CI_Controller
     $data['title'] = 'Employee List';
 
     //data karyawan yang sedang login untuk topbar
-    $data['kr'] = $this->_kr->find_username($this->session->userdata('kr_username'));
+    $data['kr'] = $this->_kr->find_by_username($this->session->userdata('kr_username'));
 
     //data karyawan untuk konten
     $data['kr_all'] = $this->_kr->return_all();
@@ -36,19 +37,49 @@ class Karyawan_CRUD extends CI_Controller
     $this->load->view('templates/footer');
     
   }
-
+  
+  public function check_default($post_string)
+  {
+    return $post_string == '0' ? FALSE : TRUE;
+  }
+  
   public function add(){
 
-    $data['title'] = 'Create Employee';
+    $this->form_validation->set_rules('kr_nama_depan', 'First Name', 'required|trim');
+		$this->form_validation->set_rules('kr_nama_belakang', 'Last Name', 'required|trim');
+		$this->form_validation->set_rules('kr_username', 'Username', 'required|trim|is_unique[kr.kr_username]',['is_unique' => 'This username already exist!']);
+		$this->form_validation->set_rules('kr_password1', 'Password', 'required|trim|min_length[3]|matches[kr_password2]',['matches' => 'Password not match', 'min_length' => 'Password too short']);
+		$this->form_validation->set_rules('kr_password2', 'Password', 'required|trim|matches[kr_password1]');
+    $this->form_validation->set_rules('kr_jabatan','Employee Role','required|callback_check_default');
+    $this->form_validation->set_message('check_default', 'You need to select something other than the default');
 
-    //data karyawan yang sedang login untuk topbar
-    $data['kr'] = $this->_kr->find_username($this->session->userdata('kr_username'));
+		if($this->form_validation->run() == false){
+			$data['title'] = 'Create Employee';
 
-    $this->load->view('templates/header',$data);
-    $this->load->view('templates/sidebar',$data);
-    $this->load->view('templates/topbar',$data);
-    $this->load->view('karyawan_crud/add',$data);
-    $this->load->view('templates/footer');
+      //data karyawan yang sedang login untuk topbar
+      $data['kr'] = $this->_kr->find_by_username($this->session->userdata('kr_username'));
+      $data['jabatan_all'] = $this->_jabatan->return_all();
+
+      $this->load->view('templates/header',$data);
+      $this->load->view('templates/sidebar',$data);
+      $this->load->view('templates/topbar',$data);
+      $this->load->view('karyawan_crud/add',$data);
+      $this->load->view('templates/footer');
+		}
+		else{
+			$data = [
+				'kr_nama_depan' => htmlspecialchars($this->input->post('kr_nama_depan', true)),
+				'kr_nama_belakang' => htmlspecialchars($this->input->post('kr_nama_belakang', true)),
+				'kr_username' => $this->input->post('kr_username'),
+				'kr_password' => password_hash($this->input->post('kr_password1'), PASSWORD_DEFAULT),
+				'kr_jabatan_id' => $this->input->post('kr_jabatan'),
+				'kr_date_created' => time()
+			];
+
+			$this->db->insert('kr', $data);
+			$this->session->set_flashdata('message','<div class="alert alert-success" role="alert">Employee Created!</div>');
+			redirect('karyawan_crud/add');
+		}
     
   }
 
--- application/views/karyawan_crud/add.php
+++ application/views/karyawan_crud/add.php
@@ -7,30 +7,56 @@
             <div class="col-lg">
             <div class="p-5">
                 <div class="text-center">
-                <h1 class="h4 text-gray-900 mb-4">Insert Employee</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Insert Employee</h1>
                 </div>
-                <form class="user" method="post" action="<?= base_url('auth/registration'); ?>">
+                
+                <?= $this->session->flashdata('message'); ?>
+
+                <form class="user" method="post" action="<?= base_url('Karyawan_CRUD/add'); ?>">
                     <div class="form-group row">
                         <div class="col-sm-6 mb-3 mb-sm-0">
-                            <input type="text" class="form-control form-control-user" id="kr_nama_depan" name="kr_nama_depan" placeholder="First Name" value="<?= set_value('kr_nama_depan') ?>">
+                            <input type="text" class="form-control" id="kr_nama_depan" name="kr_nama_depan" placeholder="First Name" value="<?= set_value('kr_nama_depan') ?>">
                             <?= form_error('kr_nama_depan','<small class="text-danger pl-3">','</small>'); ?>
                         </div>
                         <div class="col-sm-6">
-                            <input type="text" class="form-control form-control-user" id="kr_nama_belakang" name="kr_nama_belakang" placeholder="Last Name" value="<?= set_value('kr_nama_belakang') ?>">
+                            <input type="text" class="form-control" id="kr_nama_belakang" name="kr_nama_belakang" placeholder="Last Name" value="<?= set_value('kr_nama_belakang') ?>">
                             <?= form_error('kr_nama_belakang','<small class="text-danger pl-3">','</small>'); ?>
                         </div>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control form-control-user" id="kr_username" name="kr_username" placeholder="Username" value="<?= set_value('kr_username') ?>">
+                        <input type="text" class="form-control" id="kr_username" name="kr_username" placeholder="Username" value="<?= set_value('kr_username') ?>">
                         <?= form_error('kr_username','<small class="text-danger pl-3">','</small>'); ?>
                     </div>
                     <div class="form-group row">
                         <div class="col-sm-6 mb-3 mb-sm-0">
-                            <input type="password" class="form-control form-control-user" id="kr_password1" name="kr_password1" placeholder="Password">
+                            <input type="password" class="form-control" id="kr_password1" name="kr_password1" placeholder="Password">
                             <?= form_error('kr_password1','<small class="text-danger pl-3">','</small>'); ?>
                         </div>
                         <div class="col-sm-6">
-                            <input type="password" class="form-control form-control-user" id="kr_password2" name="kr_password2" placeholder="Repeat Password">
+                            <input type="password" class="form-control" id="kr_password2" name="kr_password2" placeholder="Repeat Password">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-sm-6 mb-3 mb-sm-0">
+                            
+                            <select name="kr_jabatan" id="kr_jabatan" class="form-control">
+                                <?php 
+                                    $_selected = set_value('kr_jabatan');
+                                    echo "<option value = 0>Employee Role </option>";
+                                    foreach($jabatan_all as $m) : 
+                                        if($_selected == $m['jabatan_id']){
+                                            $s = "selected";
+                                        }
+                                        else{
+                                            $s = "";
+                                        }
+                                        if($m['jabatan_id']!=1){
+                                            echo "<option value=".$m['jabatan_id']." ".$s.">".$m['jabatan_nama']."</option>";
+                                        }
+                                    endforeach 
+                                ?>
+                            </select>
+                            <?= form_error('kr_jabatan','<small class="text-danger pl-3">','</small>'); ?>
                         </div>
                     </div>
                     <button type="submit" class="btn btn-primary btn-user btn-block">
