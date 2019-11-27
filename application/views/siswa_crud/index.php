21eedc5ac0242cc14d010f8492e39e201cf670f4
Hans Kristian <hanskristianwijaya@yahoo.com>
2019-11-27 18:01:06 +0700

    perbaikan tampilan siswa

--- application/views/siswa_crud/index.php
+++ application/views/siswa_crud/index.php
@@ -7,23 +7,23 @@
         <div class="col-lg">
           <div class="p-5 overflow-auto">
             <div class="text-center">
-              <h1 class="h4 text-gray-900 mb-4">List Students Name</h1>
+              <h1 class="h4 text-gray-900 mb-4"><u>Students List</u></h1>
             </div>
             
             <?= $this->session->flashdata('message'); ?>
             
-            <a href="<?= base_url('siswa_crud/add') ?>" class="btn btn-primary mb-3">Add New Student</a>
+            <a href="<?= base_url('siswa_crud/add') ?>" class="btn btn-sm btn-primary mb-3">Add New Student</a>
 
-            <table class="table display compact table-hover dt">
+            <table class="table table-sm table-bordered display compact table-hover dt" style="font-size:14px;">
               <thead>
-                <tr>
-                  <th>Student Name</th>
-                  <th>Reg Number</th>
-                  <th>NISN</th>
-                  <th>Gender</th>
-                  <th>Religion</th>
-                  <th>Year</th>
-                  <th>Action</th>
+                <tr class="bg-dark text-white text-center">
+                  <th class="p-3">Name</th>
+                  <th style='width: 50px;' class="p-3">Reg Number</th>
+                  <th class="p-3">NISN</th>
+                  <th class="p-3">Gender</th>
+                  <th class="p-3">Religion</th>
+                  <th class="p-3">Year</th>
+                  <th class="p-3">Action</th>
                 </tr>
               </thead>
               <tbody>
@@ -46,19 +46,19 @@
                     <td><?= $m['agama_nama'] ?></td>
                     <td><?= $m['t_nama'] ?></td>
                     <td>
-                      <div class="form-group row">
+                      <div class="form-group row m-0" style="padding: 0px 0px 0px 0px;">
                         <form class="" action="<?= base_url('Siswa_CRUD/update') ?>" method="get">
                           <input type="hidden" name="_id" value=<?= $m['sis_id'] ?>>
                           <button type="submit" class="badge badge-warning">
                             Edit
                           </button>
                         </form>
-                        <form class="" action="" method="post">
+                        <!-- <form class="" action="" method="post">
                           <input type="hidden" name="" method="get" value=<?= $m['sis_id'] ?>>
                           <button type="submit" class="badge badge-danger">
                             Delete
                           </button>
-                        </form>
+                        </form> -->
                       </div>
                     </td>
                   </tr>
